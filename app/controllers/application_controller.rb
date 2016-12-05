ae983ef62fae9ef9647401fe845a2a34e41efe4c
junk83 <junkimura83@gmail.com>
2016-12-05 16:46:38 +0900

    add after sign in path and after sign out path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
 end
