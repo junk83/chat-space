1d2bcf9f0e0f96a1fdb32517afd1b4e22451bdf1
junk83 <junkimura83@gmail.com>
2016-12-06 17:01:11 +0900

    edit index action and create action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,15 @@
 class GroupsController < ApplicationController
-  def index
+  def new
+    @group = Group.new
   end
 
   def create
+    Group.create(group_params)
+    redirect_to root_path
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
