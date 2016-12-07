e61d6023deb258e73f17e90f67d28eda57e8b8bf
junk83 <junkimura83@gmail.com>
2016-12-07 18:21:13 +0900

    modify groups controller
    modify index action
    add edit action
    add update action
    add private group_list action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,10 @@
 class GroupsController < ApplicationController
+
   def index
-    @groups = Group.includes(:users).where(group_users: {user_id: current_user.id})
+    @groups = group_list
+    render layout: "chat_space"
   end
-  
+
   def new
     @group = Group.new
   end
@@ -16,7 +18,24 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.includes(:users).find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update_attributes(group_params)
+      redirect_to group_messages_path(@group), notice: 'チャットグループが更新されました。'
+    else
+      render "edit"
+    end
+  end
+
   private
+  def group_list
+    Group.includes(:users).where(group_users: {user_id: current_user.id})
+  end
+
   def group_params
     params.require(:group).permit(:name, { user_ids: [] })
   end
