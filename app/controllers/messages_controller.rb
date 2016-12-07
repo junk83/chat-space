fa6e2a2daafeb2b2bff754ffd4d5515cbb043f16
junk83 <junkimura83@gmail.com>
2016-12-07 18:20:11 +0900

    inherit groups controller and add index action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,8 @@
-class MessagesController < ApplicationController
+class MessagesController < GroupsController
+  layout "chat_space"
 
+  def index
+    @groups = group_list
+    @group = Group.find(params[:group_id])
+  end
 end
