2c14599a86725dfa413ad2b35365877946bd21b5
junk83 <junkimura83@gmail.com>
2016-12-08 18:20:20 +0900

    change use form save method to valid method

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
     let(:message) { build(:message) }
-    
+
     it "is valid with a body" do
       expect(message).to be_valid
     end
 
     it "is invalid without a body" do
       message.body = nil
-      message.save
+      message.valid?
       expect(message.errors[:body]).to include("を入力してください。")
     end
   end
