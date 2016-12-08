22859e37da8179067ac1b6bbb0505ad9949f7063
junk83 <junkimura83@gmail.com>
2016-12-08 18:18:50 +0900

    add empty line

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
     let(:message) { build(:message) }
+    
     it "is valid with a body" do
       expect(message).to be_valid
     end
