055a30336824baf1c882c1e8bbccca00ad00df06
junk83 <junkimura83@gmail.com>
2016-12-10 16:30:17 +0900

    change the order of nest

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -7,25 +7,32 @@ describe MessagesController do
   let(:message) { build(:message) }
   let(:messages) { create_list(:message, 3, user_id: user.id, group_id: group.id) }
 
-
-  describe 'GET #index' do
-    context "by anonymous user" do
+  describe "by anonymous user" do
+    describe "GET #index" do
       it "redirects to the new user session path" do
         get :index, params: { group_id: group.id }
         expect(response).to redirect_to new_user_session_path
       end
     end
 
-    context "by login user" do
-
-      before do
-        sign_in user
+    describe "POST #create" do
+      it "redirects to the new user session path" do
+        post :create, params: { message: attributes_for(:message), group_id: group.id, user_id: user.id }
+        expect(response).to redirect_to new_user_session_path
       end
+    end
+  end
 
-      after do
-        sign_out user
-      end
+  describe "by login user" do
+    before do
+      sign_in user
+    end
 
+    after do
+      sign_out user
+    end
+
+    describe "GET #index" do
       it "assigns the requested contact to @groups" do
         get :index, params: { group_id: group.id}
         expect(assigns(:groups)).to match(groups)
@@ -41,26 +48,8 @@ describe MessagesController do
         expect(response).to render_template :index
       end
     end
-  end
 
-  describe 'POST #create' do
-
-    context "by anonymous user" do
-      it "redirects to the new user session path" do
-        post :create, params: { message: attributes_for(:message), group_id: group.id, user_id: user.id }
-        expect(response).to redirect_to new_user_session_path
-      end
-    end
-
-    context "by login user" do
-      before do
-        sign_in user
-      end
-
-      after do
-        sign_out user
-      end
-
+    describe "POST #create" do
       it "assigns the requested contact to @group" do
         post :create, params: { message: attributes_for(:message), group_id: group.id, user_id: user.id }
         expect(assigns(:group)).to eq group
