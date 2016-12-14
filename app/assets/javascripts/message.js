46807204d1ff838ac9b9508ff124c79d4696b664
junk83 <junkimura83@gmail.com>
2016-12-14 20:05:35 +0900

    modify message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -20,39 +20,46 @@ $(function(){
     var html = $('.chat').before('<p class="layout-alert">' + alert + '</p>');
   }
 
-  $(document).on('submit', '.new_message', function(e) {
-    e.preventDefault();
-    $('.layout-alert').remove();
-    var form = $(this).get(0);
-    var formData = new FormData(form);
-    var url = $(this).attr('action');
-    $.ajax({
-      type: 'POST',
-      url: url + '.json',
-      data: formData,
-      processData: false,
-      contentType: false,
-      dataType: 'json',
-    })
-    .done(function(data) {
-      if(data.status == 200){
-        // 投稿メッセージのhtmlを生成し、画面上に表示する
-        var html = buildHTML(data);
-        $('.chat-messages').append(html);
-        // サイドメニューの最新メッセージを更新する
-        $('.chat-group__link[href = "' + url + '"]').find('p.chat-group__message').text(data.body);
-      }else{
-        //バリデーションエラー時のアラートメッセージを画面上に表示する
-        buildALERT(data.alert);
-      }
-      //フォームを空にする
-      form.reset();
-      //ボタンを有効化する
-      $(".chat-footer__btn").prop("disabled", false);
-    })
-    .fail(function() {
-      buildALERT("通信エラーが発生しました。");
-      $(".chat-footer__btn").prop("disabled", false);
-    });
-  });
+  // メッセージ送信クロージャ
+  var messageSubmit = function(e){
+      e.preventDefault();
+      $('.layout-alert').remove();
+      var form = $('.new_message').get(0);
+      var formData = new FormData(form);
+      var url = $('.new_message').attr('action');
+      $.ajax({
+        type: 'POST',
+        url: url + '.json',
+        data: formData,
+        processData: false,
+        contentType: false,
+        dataType: 'json',
+      })
+      .done(function(data) {
+        if(data.status == 200){
+          // 投稿メッセージのhtmlを生成し、画面上に表示する
+          var html = buildHTML(data);
+          $('.chat-messages').append(html);
+          // サイドメニューの最新メッセージを更新する
+          $('.chat-group__link[href = "' + url + '"]').find('p.chat-group__message').text(data.body);
+        }else{
+          //バリデーションエラー時のアラートメッセージを画面上に表示する
+          buildALERT(data.alert);
+        }
+        //フォームを空にする
+        form.reset();
+        //ボタンを有効化する
+        $(".chat-footer__btn").prop("disabled", false);
+      })
+      .fail(function() {
+        buildALERT("通信エラーが発生しました。");
+        $(".chat-footer__btn").prop("disabled", false);
+      });
+  };
+
+  // Sendボタンが押された場合の処理
+  $(document).on('submit', '.new_message', messageSubmit);
+  // ファイルが選択された場合の処理
+  $(document).on('change', '#message_image', messageSubmit);
+
 });
