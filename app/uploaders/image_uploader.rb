90f49748f874dc3ef18d16f2bd97af532af9e09b
junk83 <junkimura83@gmail.com>
2016-12-14 19:49:22 +0900

    add config

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,50 +1,38 @@
-# encoding: utf-8
-
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # 保存先を切り替え
+  case Rails.env
+    when 'production'
+      storage :fog
+    when 'development'
+      storage :fog
+    when 'test'
+      storage :file
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  # 保存ディレクトリを指定
+  def store_dir
+    "#{Rails.env}/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+  # 画像は300pxにリサイズ
+  process :resize_to_limit => [300, 300]
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
+  # アップロード出来るファイルを限定
   def extension_white_list
     %w(jpg jpeg gif png)
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  # 保存形式をjpgとする
+   process :convert => 'jpg'
 
+   # ファイル名を日付に変更
+   def filename
+     if original_filename.present?
+       time = Time.now
+       name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
+       name.downcase
+     end
+   end
 end
